941d0f4ca6890fd082caf1c62881f625b541575b
Dylan Johnston <qdylanj@gmail.com>
2016-03-22 11:15:14 +0100

    Add UUID helpers

--- lib/shore/client.rb
+++ lib/shore/client.rb
@@ -2,6 +2,7 @@ require 'active_support/all'
 require_relative 'client/version'
 require_relative 'client/tokens/access_token'
 require_relative 'client/messaging'
+require_relative 'client/uuid_helpers'
 
 module Shore
   module Client # :nodoc:
---
+++ lib/shore/client/uuid_helpers.rb
@@ -0,0 +1,21 @@
+module Shore
+  module Client
+    module UUID
+			UUID_FORMAT = 'H8H4H4H4H12'
+
+      # from_urlsafe("uUtUi3QLle6IvU1oOYIezg")
+			# => "b94b548b-740b-95ee-88bd-4d6839821ece"
+      def self.from_urlsafe(url)
+        url = url.gsub('_','/').gsub('-','+')
+				url.unpack('m').first.unpack(UUID_FORMAT).join('-')
+			end
+
+			# to_urlsafe('b94b548b-740b-95ee-88bd-4d6839821ece')
+		  # => "uUtUi3QLle6IvU1oOYIezg"
+      def self.to_urlsafe(uuid)
+				bytestring = uuid.split('-').pack(UUID_FORMAT)
+        Base64.urlsafe_encode64(bytestring).gsub('=','')
+			end
+    end
+  end
+end
