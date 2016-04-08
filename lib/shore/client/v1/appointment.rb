d563becda3b06c3f07910ae6a9e27a696d3bad3b
Austin Moore <am@termine24.de>
2016-04-08 14:33:56 +0200

    F [#MMW-58] Add classes exposed by `GET /v1/appointments/:id` and bump version

---
+++ lib/shore/client/v1/appointment.rb
@@ -0,0 +1,8 @@
+module Shore
+  module Client
+    module V1
+      class Appointment < CoreBase # :nodoc:
+      end
+    end
+  end
+end
--- lib/shore/client/version.rb
+++ lib/shore/client/version.rb
@@ -1,5 +1,5 @@
 module Shore
   module Client # :nodoc:
-    VERSION = '0.4.6'.freeze
+    VERSION = '0.4.7'.freeze
   end
 end
