b2cc6dc97788b3968d3f3d5ffcbb77f79913e626
Austin Moore <am@shore.com>
2016-07-06 17:01:47 +0200

    C [#SRC-3] Update the public documentation links to what they are/will be

--- lib/shore/v1/conversation.rb
+++ lib/shore/v1/conversation.rb
@@ -2,8 +2,8 @@ require_relative 'messaging_base'
 
 module Shore
   module V1
-    # TODO@am: Add `@see` link to public api documentation
     # TODO@am: Use base class `ClientBase` as soon as requestable via gateway
+    # @see https://docs.shore.com/v1/#conversations
     class Conversation < MessagingBase # :nodoc:
       has_one :merchant
       has_one :last_message
--- lib/shore/v1/customer.rb
+++ lib/shore/v1/customer.rb
@@ -2,8 +2,8 @@ require_relative 'customer_base'
 
 module Shore
   module V1
-    # TODO@am: Add `@see` link to public api documentation
     # TODO@am: Use base class `ClientBase` as soon as requestable via gateway
+    # @see https://docs.shore.com/v1/#customers
     class Customer < CustomerBase
     end
   end
