14932c696fdc46a387c6b934702ad70c9b7b45ff
Pedro Pereira <pedro.pereira@shore.com>
2017-09-25 14:12:20 +0200

    add order and payment

---
+++ lib/shore/v2/order.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'client_base'
+
+module Shore
+  module V2
+    # @see https://docs.shore.com/v2_public.html#orders
+    class Order < ClientBase
+    end
+  end
+end
