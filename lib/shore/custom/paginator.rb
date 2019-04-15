9d09bed369597af9538932aea55dda38943fbc5d
Evgeny Pavlov <evgeny@pavlov.name>
2019-04-15 15:39:13 +0200

    Correct rubocop

--- lib/shore/custom/paginator.rb
+++ lib/shore/custom/paginator.rb
@@ -4,8 +4,8 @@ module Shore
   module Custom
     # @see https://github.com/JsonApiClient/json_api_client#custom-paginator
     class Paginator < JsonApiClient::Paginating::Paginator
-      self.page_param = "number"
-      self.per_page_param = "size"
+      self.page_param = 'number'
+      self.per_page_param = 'size'
     end
   end
 end
