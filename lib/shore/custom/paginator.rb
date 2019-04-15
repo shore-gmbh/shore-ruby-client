99b61a5b962de96dedf4aff169dc8c35405bfe50
Evgeny Pavlov <evgeny@pavlov.name>
2019-04-15 15:37:48 +0200

    Add custom paginator

---
+++ lib/shore/custom/paginator.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Shore
+  module Custom
+    # @see https://github.com/JsonApiClient/json_api_client#custom-paginator
+    class Paginator < JsonApiClient::Paginating::Paginator
+      self.page_param = "number"
+      self.per_page_param = "size"
+    end
+  end
+end
