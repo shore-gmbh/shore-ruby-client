28395753fb3980827a54d2af8562d90ad095ca44
Christiano Rodrigues de Paula <christiano.rodrigues@shore.com>
2021-02-15 11:39:25 +0100

    add shifts

---
+++ lib/shore/v2/shift.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'client_base'
+
+module Shore
+  module V2
+    # @see https://docs.shore.com/v2_public.html#shifts
+    class Shift < ClientBase
+    end
+  end
+end
+
