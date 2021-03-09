41d87f7fe7e924ec7adba508242fcf1fae0666da
vtm9 <vtmilyakov@yandex.ru>
2021-03-09 13:05:49 +0300

    Add V2::Shift for availability

---
+++ lib/shore/v2/shift.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'client_base'
+
+module Shore
+  module V2
+    # @see https://apidocs.shore.com/#a44a9544-2e28-4bf6-9743-85afde8fc6c2
+    class Shift < ClientBase
+    end
+  end
+end
